c94055e7b35ff7678307df9d308cbdcca3a4e036
miguelpixel <miguel.roses.m@gmail.com>
2023-07-28 15:23:44 -0700

    construccion del chat

--- main.py
+++ main.py
@@ -1,12 +1,47 @@
-from flask import Flask, jsonify
+import os
+from flask import Flask, jsonify, request, url_for , make_response, redirect, render_template
+from dotenv import load_dotenv
+from langchain import PromptTemplate
+from langchain.chat_models import ChatOpenAI
+from langchain.document_loaders import YoutubeLoader
+from langchain.chains.summarize import load_summarize_chain
+from langchain.llms import OpenAI
+from langchain.text_splitter import RecursiveCharacterTextSplitter
+from langchain.chains.summarize import load_summarize_chain
+from langchain.llms import OpenAI
 import os
 
 app = Flask(__name__)
 
 
-@app.route('/')
+@app.route('/', methods=('GET','POST'))
 def index():
-    return jsonify({"Choo Choo": "Welcome to your Flask app 🚅"})
+    if request.method == 'POST':
+        url = request.form['url']
+        loader = YoutubeLoader.from_youtube_url(url, add_video_info=True, language = 'es')
+        result = loader.load()
+        
+        text_splitter = RecursiveCharacterTextSplitter(chunk_size=2000,chunk_overlap=0)
+        texts = text_splitter.split_documents(result)
+        texts_trim = texts[:5]
+        llm = OpenAI(temperature=0.2)
+        prompt_template = """Eres un experto en Copywriting, Creame 5 titulos de máximo 50 caracteres con el siguiente texto:
+
+        text: ```{text}```
+
+        """
+        PROMPT = PromptTemplate(template=prompt_template,input_variables=["text"])
+        chain = load_summarize_chain(llm, chain_type="stuff", prompt=PROMPT)
+        chain.run(texts_trim)
+
+        return render_template('index.html',result = chain.run(texts_trim))
+        
+
+    #user_ip = request.remote_addr
+    #response = make_response(redirect('/hello'))
+    #response.set_cookie('user_ip',user_ip)
+    result = request.args.get('url')
+    return render_template('index.html', result=result)
 
 
 if __name__ == '__main__':
